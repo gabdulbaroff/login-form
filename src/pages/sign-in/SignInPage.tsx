5c0cb9839343a01e9df3d3adb1cc8d0c077ad985
Almaz <agab@exante.eu>
2026-01-12 00:11:31 +0100

    Add vite config, initial login page component

---
+++ src/pages/sign-in/SignInPage.tsx
@@ -0,0 +1,16 @@
+// import { ContentHeader } from "../../components/ContentHeader/ContentHeader";
+// import React from "react";
+// import { SignInForm } from "./SignInForm";
+
+
+export const SignInPage = () => {
+    return (
+        <div>
+            <ContentHeader label="Sign in to your account"
+                           content="Sign in using your work email and password"
+            />
+            {/*<SignInForm />*/}
+        </div>
+    )
+}
+
