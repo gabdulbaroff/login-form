b35ad84a8fdc0d8c0cf9d6ec8ae5f89b2159558b
Almaz <agab@exante.eu>
2026-01-14 21:37:48 +0100

    Add initial sign up, reset password pages

---
+++ src/pages/reset-password/ResetPasswordPage.tsx
@@ -0,0 +1,12 @@
+import { ContentHeader } from '../../components/ContentHeader';
+
+export const ResetPasswordPage = () => {
+  return (
+    <div>
+      <ContentHeader
+        label='Stay in touch !!!'
+        content='A registration form will appear here soon.'
+      />
+    </div>
+  );
+};
--- src/router/Routes.tsx
+++ src/router/Routes.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { SignInPage } from '../pages/sign-in';
+import { ResetPasswordPage } from '../pages/reset-password';
+import { SignUpPage } from '../pages/sign-up';
 
 export const AppRoutes = () => {
   return (
     <BrowserRouter basename='/'>
       <Routes>
         <Route path='/login-form' element={<SignInPage />} />
+        <Route path='/user/reset-password' element={<ResetPasswordPage />} />
+        <Route path='/sign-up' element={<SignUpPage />} />
       </Routes>
     </BrowserRouter>
   );
