0187435d787aa00264c03d6055d9e78c8654bd2a
Almaz <agab@exante.eu>
2026-01-15 18:24:38 +0100

    Add back button

--- src/pages/sign-up/SignUpPage.tsx
+++ src/pages/sign-up/SignUpPage.tsx
@@ -2,15 +2,21 @@ import { ContentHeader } from '../../components/ContentHeader';
 import Lottie from 'lottie-react';
 import launchingSoonAnimation from '../../animations/launching-soon.json';
 import css from './SignUpPage.module.css';
+import { Button } from '../../components/Button';
+import { useNavigate } from 'react-router-dom';
 
 export const SignUpPage = () => {
+  const navigate = useNavigate();
+  const handleBack = () => {
+    navigate('/login-form');
+  };
   return (
     <div>
-      <ContentHeader
-        label='Stay in touch !!!'
-        content='A registration form will appear here soon.'
-      />
+      <ContentHeader label='Stay in touch' content='A registration form will appear here soon' />
       <Lottie animationData={launchingSoonAnimation} className={css.lottie} />
+      <Button btnType='secondary' onClick={handleBack} className={css.logoutButton}>
+        Back to sign in
+      </Button>
     </div>
   );
 };
